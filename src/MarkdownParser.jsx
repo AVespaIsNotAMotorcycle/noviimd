4e96c81f5a92bf35d2084d17c111fb4ff506e394
sam <samuelmebersole@gmail.com>
2023-02-24 11:40:04 +0100

    Refactored code, added support for <br> tag in md

--- src/MarkdownParser.jsx
+++ src/MarkdownParser.jsx
@@ -5,10 +5,10 @@ function convertHeaderAltSyntax(lines) {
   lines.forEach((line, index) => {
     if (index === 0) { nlines.push(line); return; }
     const last = nlines.length - 1;
-    if (line.search(/^[= \r\n]*$/) !== -1) {
+    if (line.search(/^[ ]*[=]+[= \r\n]*$/) !== -1) {
       nlines[last] = '# '.concat(nlines[last]);
       return;
-    } else if (line.search(/^[- \r\n]*$/) !== -1) {
+    } else if (line.search(/^[ ]*[-]+[- \r\n]*$/) !== -1) {
       nlines[last] = '## '.concat(nlines[last]);
       return;
     }
@@ -18,27 +18,105 @@ function convertHeaderAltSyntax(lines) {
 }
 
 function interpretHeader(line, index) {
-  if (line.search(/^[#]{6}/) !== -1) { return <h6 key={`S{index}`}>{line.substring(6)}</h6>; }
-  if (line.search(/^[#]{5}/) !== -1) { return <h5 key={`S{index}`}>{line.substring(5)}</h5>; }
-  if (line.search(/^[#]{4}/) !== -1) { return <h4 key={`S{index}`}>{line.substring(4)}</h4>; }
-  if (line.search(/^[#]{3}/) !== -1) { return <h3 key={`S{index}`}>{line.substring(3)}</h3>; }
-  if (line.search(/^[#]{2}/) !== -1) { return <h2 key={`S{index}`}>{line.substring(2)}</h2>; }
-  if (line.search(/^[#]{1}/) !== -1) { return <h1 key={`S{index}`}>{line.substring(1)}</h1>; }
-
+  if (line.search(/^[#]{6}/) !== -1) { return [['h6', line.substring(6)]]; }
+  if (line.search(/^[#]{5}/) !== -1) { return [['h5', line.substring(5)]]; }
+  if (line.search(/^[#]{4}/) !== -1) { return [['h4', line.substring(4)]]; }
+  if (line.search(/^[#]{3}/) !== -1) { return [['h3', line.substring(3)]]; }
+  if (line.search(/^[#]{2}/) !== -1) { return [['h2', line.substring(2)]]; }
+  if (line.search(/^[#]{1}/) !== -1) { return [['h1', line.substring(1)]]; }
+  if (line.search(/^[ ]*[=]+[= \r\n]*$/) !== -1) { return [['h1-alt', '']] }
+  if (line.search(/^[ ]*[-]+[- \r\n]*$/) !== -1) { return [['h2-alt', '']] }
   return null;
 }
 
-function interpretLine(line, index) {
-  if (interpretHeader(line, index)) { return interpretHeader(line, index); }
-
+function interpretPara(line, index) {
+  if (line.search(/^\s*$/) !== -1) { return [['end-para', '']]; }
+  if (line.search(/[  ]$/) !== -1
+     || line.search(/[<br>]$/) !== -1) {
+    return [['text', line], ['br', '']];
+  }
   return null;
+}
+
+function interpretLine(line, index, flags) {
+  if (interpretHeader(line, index)) { return interpretHeader(line, index); }
+  if (interpretPara(line, index)) { return interpretPara(line, index); }
+
+  return [['text', line]];
+}
+
+const PARA_CONTENT = ['text', 'br'];
+function condenseParagraph(tags, index) {
+  const lines = [];
+  let startIndex = index - 1;
+  for (
+    let i = index - 1;
+    (i > -1 && PARA_CONTENT.includes(tags[i][0]));
+    i -= 1
+  ) {
+    const line = tags[i];
+    switch (line[0]) {
+      case 'text':
+        lines.push(line[1]);
+        break;
+      case 'br':
+        lines.push(<br key={`br-${i}`}/>);
+        break;
+      default:
+        return;
+    }
+  }
+  if (lines.length === 0) { return null; }
+  return <p key={`p-${index}`}>{lines.reverse()}</p>
+}
+
+function processIntertags(intertags) {
+  const tags = [];
+  intertags.forEach((intertag, index) => {
+    switch (intertag[0]) {
+      case 'end-para':
+        tags.push(condenseParagraph(intertags.splice(0, index), index));
+        break;
+      case 'h2-alt':
+        tags.push(<h2 key={`h2-${index - 1}`}>{intertags[index - 1][1]}</h2>);
+        break;
+      case 'h1-alt':
+        tags.push(<h1 key={`h1-${index - 1}`}>{intertags[index - 1][1]}</h1>);
+        break;
+      case 'h6':
+        tags.push(<h6 key={`h6-${index}`}>{intertag[1]}</h6>);
+        break;
+      case 'h5':
+        tags.push(<h5 key={`h5-${index}`}>{intertag[1]}</h5>);
+        break;
+      case 'h4':
+        tags.push(<h4 key={`h4-${index}`}>{intertag[1]}</h4>);
+        break;
+      case 'h3':
+        tags.push(<h3 key={`h3-${index}`}>{intertag[1]}</h3>);
+        break;
+      case 'h2':
+        tags.push(<h2 key={`h2-${index}`}>{intertag[1]}</h2>);
+        break;
+      case 'h1':
+        tags.push(<h1 key={`h1-${index}`}>{intertag[1]}</h1>);
+        break;
+      default:
+        return;
+    }
+  });
+  return tags;
 }
 
 function MarkdownParser({ md }) {
   let lines = md.split('\n');
-  lines = convertHeaderAltSyntax(lines);
-  const tags = lines.map((line, index) => interpretLine(line, index));
-  return tags;
+  let intertags = [];
+  lines.forEach((line, index) => {
+    const linetags = interpretLine(line, index);
+    linetags.forEach((linetag) => { intertags.push(linetag); });
+  });
+  intertags.push(['end-para', ''])
+  return processIntertags(intertags);
 }
 
 export default MarkdownParser;
--- src/MarkdownParser.test.js
+++ src/MarkdownParser.test.js
@@ -53,10 +53,55 @@ describe('renders individual lines', () => {
     expect(header.nodeName).toBe('H1');
   });
 
-  test('renders header 2', () => {
+  test('renders header 2 alt syntax', () => {
     const md = `${lineContent}\n------`;
     render(<MarkdownParser md={md} />);
     const header = screen.getByText(lineContent);
     expect(header.nodeName).toBe('H2');
   });
 })
+
+describe('paragraphs', () => {
+  test('create 2 p tags', () => {
+    const md = `one\ntwo\n\nthree\nfour`;
+    render(<MarkdownParser md={md} />);
+    const p11 = screen.getByText(/one/);
+    const p12 = screen.getByText(/two/);
+    expect(p11).toBe(p12);
+    const p21 = screen.getByText(/three/);
+    const p22 = screen.getByText(/four/);
+    expect(p21).toBe(p22);
+  });
+
+  test('one p tag w br via spaces', () => {
+    const md = `one\ntwo  \nthree\nfour`;
+    render(<MarkdownParser md={md} />);
+    const p22 = screen.getByText(/four/);
+    let hasbr = false;
+    Object.keys(p22).forEach((key) => {
+      const val = p22[key];
+      if (val.children) {
+        Object.keys(val.children).forEach((chKey) => {
+          if (val.children[chKey].type === 'br') { hasbr = true; }
+        });
+      }
+    });
+    expect(hasbr).toBe(true);
+  })
+
+  test('one p tag w br via <br>', () => {
+    const md = `one\ntwo<br>\nthree\nfour`;
+    render(<MarkdownParser md={md} />);
+    const p22 = screen.getByText(/four/);
+    let hasbr = false;
+    Object.keys(p22).forEach((key) => {
+      const val = p22[key];
+      if (val.children) {
+        Object.keys(val.children).forEach((chKey) => {
+          if (val.children[chKey].type === 'br') { hasbr = true; }
+        });
+      }
+    });
+    expect(hasbr).toBe(true);
+  })
+});
