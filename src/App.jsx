c03199426daff2ed0f523297672d5053080332e5
sam <samuelmebersole@gmail.com>
2023-02-25 15:15:16 +0100

    Ui mods

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,19 @@ function App() {
   const [markdown, setMarkdown] = useState('');
 
   return (
-    <div className="App">
-      <TextField
-        onChange={(e) => {
-          setMarkdown(e.target.value);
-        }}
-        placeholder="Enter some markdown here!"
-      />
-      <MarkdownParser md={markdown} />
+    <div className="flex-container">
+      <div className="flex-item">
+        <TextField
+          onChange={(e) => {
+            setMarkdown(e.target.value);
+          }}
+          placeholder="Enter some markdown here!"
+        />
+      </div>
+      <div className="flex-divider" />
+      <div className="flex-item">
+        <MarkdownParser md={markdown} />
+      </div>
     </div>
   );
 }
